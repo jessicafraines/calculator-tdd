7ba31d4698430e8c39ce63ca8f5829d568d3ab82
jaclynpasquale <jaclynpasquale@gmail.com>
2014-11-12 21:30:06 -0600

    implemented decimal.js library

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-
-function displayOutput(){
+// display output text in jquery selector of #displayoutput
+function displayOutput() {
   return $('#displayoutput').text();
 }
-$(document).ready(function(){
-  displayOutput();
-  $('span').click(function(){
+
+$(document).ready(function() {
+  //displayOutput();
+  $('span').click(function() {
+    //console.log($(this)); this is the span screen
     press($(this).text());
   });
 });
@@ -17,29 +19,49 @@ $(document).ready(function(){
 var previousResult;
 var nextOperation;
 
-function add(a, b){
-  return Math.round((a + b) * 1e12) / 1e12;
+function add(a, b) {
+  var left = new Decimal(a);
+  var right = new Decimal(b);
+  //var result = a.minus(b);
+  return (left.plus(right));
+  //return Math.round((a + b) * 1e12) / 1e12;
 }
-function subtract(a, b){
-  return Math.round((a - b) * 1e12) / 1e12;
+
+function subtract(a, b) {
+  var left = new Decimal(a);
+  var right = new Decimal(b);
+  return (left.minus(right));
+  //return Math.round((a - b) * 1e12) / 1e12;
 }
-function multiply(a, b){
-  return Math.round((a * b) * 1e12) / 1e12;
+
+function multiply(a, b) {
+  var left = new Decimal(a);
+  var right = new Decimal(b);
+  console.log(left.times(right));
+  //return (left.times(right));
+  //return Math.round((a * b) * 1e12) / 1e12;
 }
-function divide(a, b){
-  return Math.round((a / b) * 1e12) / 1e12;
+
+function divide(a, b) {
+  var left = new Decimal(a);
+  var right = new Decimal(b);
+  return (left.dividedBy(right));
+  //return Math.round((a / b) * 1e12) / 1e12;
 }
-function currentValue(string){
+
+function currentValue(string) {
   return $('#displayoutput').text() * 1;
 }
-function calculate(){
-  if(!!nextOperation){
+
+function calculate() {
+  if (!!nextOperation) {
     previousResult = nextOperation(previousResult, currentValue());
-  }else{
+  } else {
     previousResult = currentValue();
   }
 }
-function press(buttonValue){
+
+function press(buttonValue) {
   switch (buttonValue) {
     case '+':
       calculate();
@@ -47,36 +69,36 @@ function press(buttonValue){
       $('#displayoutput').text('');
       break;
     case '-':
-        calculate();
-        nextOperation = subtract;
-        $('#displayoutput').text('');
-        break;
-      case '*':
-        calculate();
-        nextOperation = multiply;
-        $('#displayoutput').text('');
-        break;
-      case '/':
-        calculate();
-        nextOperation = divide;
-        $('#displayoutput').text('');
-        break;
-      case 'C':
-        calculate();
-        previousResult = 0;
-        $('#displayoutput').text(previousResult);
-        break;
-      case '=':
-        calculate();
-        $('#displayoutput').text(previousResult);
-        break;
-      case '+/-':
-        // handle +/-
-        break;
-      default:
-        var current =  $('#displayoutput').text();
-        console.log('c', current);
-        console.log('BV', buttonValue);
-        $('#displayoutput').text(current + buttonValue);
+      calculate();
+      nextOperation = subtract;
+      $('#displayoutput').text('');
+      break;
+    case '*':
+      calculate();
+      nextOperation = multiply;
+      $('#displayoutput').text('');
+      break;
+    case '/':
+      calculate();
+      nextOperation = divide;
+      $('#displayoutput').text('');
+      break;
+    case 'C':
+      calculate();
+      previousResult = "";
+      $('#displayoutput').text(previousResult);
+      break;
+    case '=':
+      calculate();
+      $('#displayoutput').text(previousResult);
+      break;
+    case '+/-':
+      // handle +/-
+      break;
+    default:
+      var current = $('#displayoutput').text();
+      //console.log('c', current);
+      //console.log('BV', buttonValue);
+      $('#displayoutput').text(current + buttonValue);
   }
 }
